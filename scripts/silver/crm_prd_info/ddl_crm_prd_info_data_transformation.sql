519d14917c81238ec6942e62ef5235e4f0eca83e
Łukasz Hetman <94021441+LukHet@users.noreply.github.com>
2025-08-15 02:08:21 +0200

    Create ddl_crm_prd_info_data_transformation.sql

---
+++ scripts/silver/crm_prd_info/ddl_crm_prd_info_data_transformation.sql
@@ -0,0 +1,26 @@
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+) 
+	SELECT
+		prd_id,
+		REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+		SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+		prd_nm,
+		ISNULL(prd_cost, 0) AS prd_cost,
+		CASE UPPER(TRIM(prd_line))
+			WHEN 'M' THEN 'Mountain'
+			WHEN 'R' THEN 'Road'
+			WHEN 'S' THEN 'Other sales'
+			WHEN 'T' THEN 'Touring'
+			ELSE 'n/a'
+		END AS prd_line,
+		CAST(prd_start_dt AS DATE) AS prd_start_dt,
+		CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
+	FROM BRONZE.crm_prd_info WHERE SUBSTRING(prd_key, 7, LEN(prd_key)) NOT IN (SELECT sls_prd_key FROM bronze.crm_sales_details)
